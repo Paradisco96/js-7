e8a0b90334e5d7e76b97ee80333111b90939120b
Paradisco96 <mr.wolf.dsgn@gmail.com>
2025-07-26 14:49:30 +0300

    feat: pass tests

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@
 */
 
 function sumArray(numbers) {
-  // code here
+  const res = numbers.reduce((prev, curr) => prev + curr, 0) //curr на первой итерации получает значение элемента массива, который находится под индексом 0.
+  return res
 }
 
 // Використання функції
@@ -14,22 +15,22 @@ function sumArray(numbers) {
 // const sum = sumArray(exampleArray)
 // console.log('Сума елементів масиву:', sum) // Виведення суми
 
-
 /*
  * #2
  *
- * Розробити функцію, яка використовує метод map масиву для створення нового масиву, в якому кожен елемент буде вдвічі більшим за елементи вхідного масиву чисел.
-*/
+ * Розробити функцію, яка використовує метод map масиву для створення нового масиву, в якому кожен елемент буде вдвічі більшим за елементи вхідного
+ * масиву чисел.
+ */
 
 function doubleArrayElements(numbers) {
-  // code here
+  const doubleElements = numbers.map((el) => el * 2)
+  return doubleElements
 }
 
 // Використання функції
 // const exampleArray = [1, 2, 3, 4, 5]
 // const doubledArray = doubleArrayElements(exampleArray)
 // console.log('Подвоєні елементи масиву:', doubledArray) // Виведення подвоєних елементів
-
 
 /*
  * #3
@@ -49,11 +50,32 @@ function doubleArrayElements(numbers) {
  * - Клас має бути модульним і здатним до використання в інших частинах програми, тому він повинен бути експортований.
  * - Клас має забезпечувати легке управління навичками, включаючи додавання нових навичок та отримання списку всіх наявних навичок.
  * - Код має бути написаний з урахуванням принципів чистого коду, забезпечуючи читабельність та легкість підтримки.
-*/
+ */
 
 class SkillsManager {
-  // code here
+  constructor() {
+    this.skills = new Array()
+  }
+
+  addSkill(skill) {
+    if (typeof skill === 'string' && skill.length > 2) {
+      this.skills.push(skill)
+      return skill
+    } else {
+      return null
+    }
+  }
+
+  getAllSkills() {
+    return this.skills
+  }
 }
+
+const skillsManager = new SkillsManager()
+
+console.log(skillsManager.addSkill('JavaScript'))
+console.log(skillsManager.addSkill('CSS'))
+console.log(skillsManager.getAllSkills())
 
 // const skillsManager = new SkillsManager()
 //
@@ -61,11 +83,11 @@ class SkillsManager {
 // console.log(skillsManager.addSkill('CSS'))
 // console.log(skillsManager.getAllSkills())
 
-
 /*
  * #4
  * Задача: Калькулятор дат.
- * Завдання: Створити модуль на JavaScript, який імплементує функцію-конструктор DateCalculator для створення об'єктів, здатних керувати датами. Калькулятор дат має надавати такі можливості:
+ * Завдання: Створити модуль на JavaScript, який імплементує функцію-конструктор DateCalculator для створення об'єктів,
+ * здатних керувати датами. Калькулятор дат має надавати такі можливості:
  * Додавання днів: Метод addDays приймає кількість днів як аргумент і додає цю кількість до поточної дати об'єкта.
  * Віднімання днів: Метод subtractDays приймає кількість днів як аргумент і віднімає цю кількість від поточної дати об'єкта.
  * Отримання результату: Метод getResult повертає поточну дату об'єкта у форматі "YYYY-MM-DD".
@@ -77,18 +99,27 @@ class SkillsManager {
  */
 
 function DateCalculator(initialDate) {
-  this.addDays = function(days) {
-    // code here
+  this.initialDate = initialDate;
+this.initialDate = new Date(this.initialDate); 
+
+  this.addDays = function (days) {
+this.initialDate.setDate(this.initialDate.getDate() + days)
+ }
+  
+  this.subtractDays = function (days) {
+this.initialDate.setDate(this.initialDate.getDate() - days)
   }
 
-  this.subtractDays = function(days) {
-    // code here
-  }
-
-  this.getResult = function() {
-    // code here
-  }
+  this.getResult = function () {
+return this.initialDate.toISOString().slice(0, 10); 
+  };
 }
+const dateCalculator = new DateCalculator('2023-01-01')
+dateCalculator.addDays(5)
+console.log(dateCalculator.getResult()) // Виводить нову дату після додавання днів
+
+dateCalculator.subtractDays(3)
+console.log(dateCalculator.getResult()) // Виводить нову дату після віднімання днів
 
 // Демонстрація використання
 // const dateCalculator = new DateCalculator('2023-01-01')
